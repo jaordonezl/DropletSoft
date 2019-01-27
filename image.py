0e332809b376acf49ef920e10cb0bf920e776c17
Javier Ordoñez <jaordonezl@unal.edu.co>
2019-01-27 09:01:53 +0300

    imagen prueba

--- image.py
+++ image.py
@@ -9,8 +9,22 @@ import cv2
 import numpy as np
 
 img = cv2.imread('img.jpg')
-retval, threshold = cv2.threshold(img, 12, 255, cv2.THRESH_BINARY)
+#retval, threshold = cv2.threshold(img, 12, 255, cv2.THRESH_BINARY)
+#cv2.imshow('original',img)
+#cv2.imshow('threshold',threshold)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+grayscaled = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#retval, threshold = cv2.threshold(grayscaled, 10, 155, cv2.THRESH_BINARY)
+#cv2.imshow('original',grayscaled)
+#cv2.imshow('threshold',threshold)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+
+retval2,threshold2 = cv2.threshold(grayscaled,125,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 cv2.imshow('original',img)
-cv2.imshow('threshold',threshold)
+cv2.imshow('Otsu threshold',threshold2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
